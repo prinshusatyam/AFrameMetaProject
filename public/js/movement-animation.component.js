51850cd88e968ab115faf1cf74e27eb08029ea5f
satyam <satyam.gupta6@cognizant.com>
2023-05-08 17:52:52 +0530

    inplace animation walking on click

--- public/js/movement-animation.component.js
+++ public/js/movement-animation.component.js
@@ -1,22 +1,56 @@
 AFRAME.registerComponent('change-animation-on-keypress', {
     schema: {
-        triggerKeyCode: {default: 32} // spacebar
+        moveforwardCode: {default: 87}, // w
+        moveleftCode: {default: 65}, //a
+        movebackwardCode: {default: 83},
+        moverightCode: {default: 68}
       },
     
       init: function() {
         var that = this;
         this.isWalking = false;
-        this.playerModelIdle = document.querySelector('#player-model');
+        this.playerModelIdle = document.querySelector('.head');
         document.body.onkeyup = function(e){
-          if(e.keyCode == that.data.triggerKeyCode){
-            that.shoot();
+          if(e.keyCode == that.data.moveforwardCode){
+            that.forwaedMovement();
+          }
+          else if(e.keyCode == that.data.moveleftCode){
+            that.leftMovement();
+          }
+          else if(e.keyCode == that.data.movebackwardCode){
+            that.backwardMovement();
+          }
+          else if(e.keyCode == that.data.moverightCode){
+            that.rightMovement();
           }
         } 
       },
-    shoot: function () {
-        console.log("hii");
-        this.playerModelIdle.removeAttribute('gltf-model','animation-mixer');
-        console.log("Unshoot!");
-        this.playerModelIdle.setAttribute('gltf-model', '#player-model-walking');
-    }
+        forwaedMovement: function () {
+            console.log("hii");
+            console.log(this.playerModelIdle)
+            console.log("Unshoot!");
+            this.playerModelIdle.setAttribute('gltf-model','assets/WAlking.glb')
+            console.log(this.playerModelIdle)
+        },
+        leftMovement: function () {
+            console.log("hii");
+            console.log(this.playerModelIdle)
+            console.log("Unshoot!");
+            this.playerModelIdle.setAttribute('gltf-model','assets//WalkStrafeLeft.glb')
+            console.log(this.playerModelIdle)
+        },
+        backwardMovement: function () {
+            console.log("hii");
+            console.log(this.playerModelIdle)
+            console.log("Unshoot!");
+            this.playerModelIdle.setAttribute('gltf-model','assets/WalkBackward.glb')
+            console.log(this.playerModelIdle)
+        },
+        rightMovement: function () {
+          console.log("hii");
+          console.log(this.playerModelIdle)
+          console.log("Unshoot!");
+          this.playerModelIdle.setAttribute('gltf-model','assets/WalkStrafeRight.glb')
+          console.log(this.playerModelIdle)
+        }
   });
