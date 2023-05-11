7c1b804db74431fad42ce9dfba10f43ad58c2799
satyam <satyam.gupta6@cognizant.com>
2023-05-11 17:22:52 +0530

    AvatarLoader Basic

---
+++ public/js/AvatarLoader.js
@@ -0,0 +1,11 @@
+const DisplayplayerModel = document.getElementById('avatar-canvas');
+const RemyplayerModel = document.getElementById('player-model');
+const RexplayerModel = document.getElementById('test-model');
+const nextAvatarButton = document.getElementById('next-avatar-button');
+
+let currentModel = DisplayplayerModel;
+nextAvatarButton.addEventListener('click', () => {
+    console.log(currentModel);
+    currentModel.setAttribute('gltf-model','assets/TRex.glb');
+    currentModel.setAttribute('scale', {x:0.1, y:0.1, z:0.1});
+});
